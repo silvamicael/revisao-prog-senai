fbb592f43954645833a71904cdac9fc00da731b4
silvamicael <micaelmadri@gmail.com>
2026-02-02 22:05:23 -0300

    att helloreact

---
+++ helloreact/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Header';
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Header />
+      <h1>HELLO REACT</h1>
+    </>
+  )
+}
+
+export default App
---
+++ helloreact/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+function Header() {
+
+    return(
+        <header>Cabeçalho da página</header>
+    )
+}
+
+
+export default Header;
